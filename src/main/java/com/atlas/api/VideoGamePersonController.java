25c826f92cce0c63aa1d57cc3dd006fa21c1718b
Jugraj Bath <jugraj.bath@gmail.com>
2020-03-21 16:25:47 -0700

    Feature - VideogamePerson & InGameCharacter

---
+++ src/main/java/com/atlas/api/VideoGamePersonController.java
@@ -0,0 +1,53 @@
+package com.atlas.api;
+
+import com.atlas.model.VideoGamePerson;
+import com.atlas.service.VideoGamePersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("app/videogameperson")
+@RestController
+public class VideoGamePersonController {
+    private final VideoGamePersonService vgPersonService;
+
+    @Autowired
+    public VideoGamePersonController(VideoGamePersonService vgPersonService) {
+        this.vgPersonService = vgPersonService;
+    }
+
+    @PostMapping
+    public void addVideoGamePerson(@Valid @NonNull @RequestBody VideoGamePerson person) {
+        vgPersonService.addVideoGame(person);
+    }
+
+    @GetMapping
+    public List<VideoGamePerson> getAllVideoGamePeople() {
+        return vgPersonService.getAllVideoGamePeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public VideoGamePerson getVideoGamePersonById(@PathVariable("id") int id) {
+        return vgPersonService.getVideoGamePersonById(id).orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteVideoGamePerson(@PathVariable("id") int id) {
+        vgPersonService.deleteVideoGamePerson(id);
+    }
+
+    @PutMapping(path="{id}")
+    public void updateVideoGamePerson(@PathVariable("id") int id,
+                                     @Valid @NonNull @RequestBody VideoGamePerson person) {
+        vgPersonService.updateVideoGamePerson(id, person);
+    }
+
+
+}
