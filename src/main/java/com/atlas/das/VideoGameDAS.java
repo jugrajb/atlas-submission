335b53733af73b7aa6236126af67a6da1d3bbf63
Jugraj Bath <jugraj.bath@gmail.com>
2020-03-20 23:38:31 -0700

    Misc - Updated Query Structure

--- src/main/java/com/atlas/das/VideoGameDAS.java
+++ src/main/java/com/atlas/das/VideoGameDAS.java
@@ -5,6 +5,8 @@ import com.atlas.dao.VideoGameDAO;
 import com.atlas.model.VideoGame;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
@@ -14,30 +16,34 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository("postgres")
-public class VideoGameDataAccessService implements VideoGameDAO {
+public class VideoGameDAS implements VideoGameDAO {
 
   private final JdbcTemplate jdbcTemplate;
+  private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
   @Autowired
-  public VideoGameDataAccessService(JdbcTemplate jdbcTemplate) {
+  public VideoGameDAS(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
+    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
   }
 
   @Override
   public int insertVideoGame(VideoGame videoGame) {
+    final String sql =
+            "INSERT INTO videogame " +
+            "(title, releaseDate, genre, pcid, sid, pid) " +
+            "VALUES (:title, :releaseDate, :genre, :pcid, :sid, :pid)";
 
-    final String sql = "INSERT INTO videogame (title, releaseDate, genre, pcid, sid, pid) VALUES (?, ?, ?, ?, ?, ?)";
+    MapSqlParameterSource args = new MapSqlParameterSource();
+    args.addValue("title", videoGame.getTitle());
+    args.addValue("releaseDate", videoGame.getReleaseDate());
+    args.addValue("genre", videoGame.getGenre());
+    args.addValue("pcid", videoGame.getPcid());
+    args.addValue("sid", videoGame.getSid());
+    args.addValue("pid", videoGame.getPid());
 
     try {
-      jdbcTemplate.update(
-              sql,
-              videoGame.getTitle(),
-              videoGame.getReleaseDate(),
-              videoGame.getGenre(),
-              videoGame.getPcid(),
-              videoGame.getSid(),
-              videoGame.getPid()
-      );
+        namedParameterJdbcTemplate.update(sql,args);
     } catch (Exception e) {
       return 0;
     }
---
+++ src/main/java/com/atlas/service/Users.java
@@ -0,0 +1,4 @@
+package com.atlas.service;
+
+public class Users {
+}
