801473b157e5436fef99377ec1fcbfe9cf934e78
Irvino Djuana <irvino.djuana@gmail.com>
2020-03-21 21:59:20 -0700

    Fix - added json property fields to model class (somehow worked without it?)

--- src/main/java/com/atlas/model/PlayableOn.java
+++ src/main/java/com/atlas/model/PlayableOn.java
@@ -1,10 +1,13 @@
 package com.atlas.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PlayableOn {
     private int gid;
     private int gpid;
 
-    public PlayableOn(int gid, int gpid) {
+    public PlayableOn(@JsonProperty("gid") int gid,
+                      @JsonProperty("gpid") int gpid) {
         this.gid = gid;
         this.gpid = gpid;
     }
