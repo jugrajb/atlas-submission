f399fbef0f9eab860616ee4f35802a444e766c0d
Irvino Djuana <irvino.djuana@gmail.com>
2020-03-21 22:02:21 -0700

    Fix - added json preperty fields for workedon

--- src/main/java/com/atlas/model/WorkedOn.java
+++ src/main/java/com/atlas/model/WorkedOn.java
@@ -1,11 +1,15 @@
 package com.atlas.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class WorkedOn {
     private int gid;
     private int pid;
     private String role;
 
-    public WorkedOn(int gid, int pid, String role) {
+    public WorkedOn(@JsonProperty("gid") int gid,
+                    @JsonProperty("pid") int pid,
+                    @JsonProperty("role") String role) {
         this.gid = gid;
         this.pid = pid;
         this.role = role;
